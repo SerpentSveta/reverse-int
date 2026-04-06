9ef846b92af65e10e92b72a595ae98eea5008625
SerpentSveta <sveta.aleksandrova@gmail.com>
2026-04-06 16:14:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const positiveN = n < 0 ? -n : n;
+  const nString = positiveN.toString();
+  let newString = '';
+  for (let i = nString.length - 1; i >= 0; i -= 1) {
+    newString += nString[i];
+  }
+  return +newString;
 };
